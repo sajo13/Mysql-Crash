afea30ea5dc7b90ab514217d28b7fdacd9b9410f
sajo13 <sajosunny007@gmail.com>
2025-01-24 11:01:52 +0530

    Add Department and Employees Table
    - Insert the sample records to corresponding Table.

--- Sql/queries.sql
+++ Sql/queries.sql
@@ -5,3 +5,39 @@ CREATE DATABASE database mysql_crash;
 
 #selecting the database
 use mysql_crash;
+
+
+-- Create 'departments' table with a Primary Key
+CREATE TABLE departments (
+                             department_id INT AUTO_INCREMENT,
+                             department_name VARCHAR(100) NOT NULL,
+                             PRIMARY KEY (department_id)
+);
+
+-- Create 'employees' table with a Foreign Key referencing 'departments'
+CREATE TABLE employees (
+                           employee_id INT AUTO_INCREMENT,
+                           first_name VARCHAR(100),
+                           last_name VARCHAR(100),
+                           department_id INT,
+                           PRIMARY KEY (employee_id),
+                           FOREIGN KEY (department_id) REFERENCES departments(department_id)
+);
+
+
+-- Insert some departments
+INSERT INTO departments (department_name) VALUES ('HR');
+INSERT INTO departments (department_name) VALUES ('Engineering');
+INSERT INTO departments (department_name) VALUES ('Marketing');
+
+-- Insert employees with foreign key references to departments
+INSERT INTO employees (first_name, last_name, department_id)
+VALUES ('Sajo', 'Sunny', 1); -- John is in HR (department_id 1)
+
+INSERT INTO employees (first_name, last_name, department_id)
+VALUES ('James', 'Smith', 2); -- Jane is in Engineering (department_id 2)
+
+INSERT INTO employees (first_name, last_name, department_id)
+VALUES ('Arun', 'Thomas', 1); -- Alex is in HR (department_id 1)
+
+
